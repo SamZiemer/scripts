2ca7b461ab19f8257d502c3f63e5431662b31b64
Sam Ziemer <samuel.ziemer@liferay.com>
2016-05-23 10:04:05 -0700

    update find_orphaned_files to find files without a database entry and entries without a file system entry

--- groovyScripts/find_orphaned_files.groovy
+++ groovyScripts/find_orphaned_files.groovy
@@ -2,52 +2,12 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil
 import com.liferay.portal.kernel.util.GetterUtil
 import com.liferay.portal.kernel.util.StringBundler
 import com.liferay.portal.security.auth.CompanyThreadLocal
-import com.liferay.portal.service.GroupLocalServiceUtil
 import com.liferay.portal.util.PropsValues
-import com.liferay.portlet.documentlibrary.model.DLFolderConstants
-import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil
+import com.liferay.portlet.documentlibrary.model.DLFileVersion
 import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil
-import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil
 import com.liferay.portlet.documentlibrary.store.DLStoreUtil
 
-// Populate caches to speed up scripts
-
-folders = DLFolderLocalServiceUtil.getDLFolders(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-fileEntries = DLFileEntryLocalServiceUtil.getDLFileEntries(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-StringBundler sb = new StringBundler();
-
-sb.append("<h1>Missing folders</h1>");
-sb.append("<table border=1><tr><th>FolderId</th><th>FileEntryId</th><th></th></tr>");
-
-// Check folders that are now missing a folder, as the document will no longer
-// be accessible to users normally. This hints at a folder that was deleted but
-// where the folders failed to be deleted.
-
-
-long missingFolderCount = 0;
-
-for (fileEntry in fileEntries) {
-	if (fileEntry.folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
-		continue
-	}
-
-	folder = DLFolderLocalServiceUtil.fetchDLFolder(fileEntry.folderId);
-
-	if (folder == null) {
-		sb.append("<tr>");
-		sb.append("<td>" + fileEntry.folderId + "</td>");
-		sb.append("<td>" + fileEntry.fileEntryId + "</td>");
-		sb.append("</tr>");
-		missingFolderCount++;
-		continue
-	}
-
-}
-sb.append("<tr><td><b>Number of missing Folders</b></td><td>" + missingFolderCount + "</td></tr>");
-sb.append("</table>");
-
-sb.append("<h1></h1>");
+public StringBundler sb = new StringBundler();
 
 // Check versions that do not have a downloadable file -- essentially, documents
 // that are supposed to exist, but won't be available for download.
@@ -58,10 +18,14 @@ sb.append("<h1>Entries missing corresponding file system Entry</h1>");
 
 sb.append("<table border=1><tr><th>GroupId</th><th>FolderId</th><th>FileEntryId</th><th>FileVersion</th></tr>");
 
+ArrayList<DLFileVersion> orphanedEntries = new ArrayList<DLFileVersion>();
+
 for (fileVersion in fileVersions) {
 	if (DLStoreUtil.hasFile(fileVersion.companyId, fileVersion.fileEntry.dataRepositoryId, fileVersion.fileEntry.name, fileVersion.version)) {
 		continue;
 	}
+
+	orphanedEntries.add(fileVersion);
 
 	groupId = fileVersion.groupId;
 	folderId = fileVersion.folderId;
@@ -73,50 +37,58 @@ for (fileVersion in fileVersions) {
 
 sb.append("</table>");
 
-// Check files on the file system that no longer have a corresponding database
-// entry -- essentially, documents that were deleted in the database but failed
-// to be deleted on the file system.
-
 rootDir = new File(PropsValues.DL_STORE_FILE_SYSTEM_ROOT_DIR);
-
 companyId = CompanyThreadLocal.companyId;
 companyDir = new File(rootDir, String.valueOf(companyId));
 
-sb.append("<h1>File System Entries with no corresponding database entry</h1>");
-
-sb.append("<table border=1><tr><th>CompanyId</th><th>FolderId</th><th>FileEntryId</th></tr>");
+Map<String, File> fileNames = new HashMap();
 
 for (dlFolderDir in companyDir.listFiles()) {
-	folderId = GetterUtil.getLong(dlFolderDir.name);
-	folder = DLFolderLocalServiceUtil.fetchDLFolder(folderId);
-
-	if (folder != null) {
-		// It might correspond to the root folder for a site, so check to see if
-		// there is a site with this id as well before we report that it is
-		// a folder that is orphaned
-
-		groupId = folder.groupId;
-		group = GroupLocalServiceUtil.fetchGroup(groupId);
-
-		if (group == null) {
-			sb.append("<tr><td>" + companyId + "</td><td>" + folderId + "</td></tr>");
-
-			continue;
-		}
-	}
-
-	for (dlFileEntryDir in dlFolderDir.listFiles()) {
-		fileEntryId = GetterUtil.getLong(dlFileEntryDir.name);
-		fileEntry = DLFileEntryLocalServiceUtil.fetchDLFileEntry(fileEntryId);
-
-		if (fileEntry == null) {
-			sb.append("<tr><td>" + companyId + "</td><td>" + folderId + "</td><td>" + fileEntryId + "</td></tr>")
-		}
-	}
-
+	findFiles(dlFolderDir, sb, fileNames);
 }
 
-sb.append("<tr /></table>");
+sb.append("<h1>Entries with file in another location</h1><table border=1><tr><th>Orphaned Entries Name</th><th>Orphaned Entry file ID</th><th>File Name</th><th>Path to File</th></tr>");
+
+for (DLFileVersion entry : orphanedEntries) {
+	file = fileNames.get(entry.getFileEntry().name);
+	if (index != null) {
+		sb.append("<tr><td>");
+		sb.append(entry.getFileEntry().name);
+		sb.append("</td><td>");
+		sb.append(entry.fileEntryId);
+		sb.append("</td><td>");
+		sb.append(file.getName());
+		sb.append("</td><td>");
+		sb.append(file.parentFile.absolutePath);
+		sb.append("</td></tr>");
+	}
+}
+sb.append("</table>");
 
 out.println(sb.toString());
 
+public void findFiles(File file, StringBundler sb, Map<String, File> fileNames) {
+
+	if (file.isDirectory()) {
+		if(file.name == (String)0) {
+			return;
+		} else if (GetterUtil.getLong(file.name) == companyId) {
+			return;
+		}
+
+		for (dlFile in file.listFiles()) {
+			findFiles(dlFile, sb, fileNames);
+		}
+	} else {
+		fileName = file.name;
+		index = fileName.indexOf("_");
+
+		if (index == -1) {
+			return;
+		} else {
+			dlFileEntryName = fileName.substring(0, index);
+		}
+
+		fileNames.put(dlFileEntryName, file);
+	}
+}
