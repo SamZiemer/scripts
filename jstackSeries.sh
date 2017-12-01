1e3d49edf81272488394b516d1046f003dc6f7e3
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2017-12-01 11:32:20 -0800

    Fix jstack script

--- jstackSeries.sh
+++ jstackSeries.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
+if [[ $# -eq 0 ]]; then
 	pid=$(jps | grep -oP '\d+ Bootstrap' | grep -oP '\d+')
-	user=${whoami}
-	count=${3:-10}
-	delay=${4:-0.5}
+	count=${2:-10}
+	delay=${3:-0.5}
 
-elif [ $1 == '--help' ] || [ $1 == '-h' ]; then
-	echo "Usage: jstackSeries <pid> <run_user> [ <count> [ <delay> ] ]"
+elif [[ $1 == '--help' ]] || [[ $1 == '-h' ]]; then
+	echo "Usage: jstackSeries <pid> [ <count> [ <delay> ] ]"
 	echo "    Defaults: count = 10, delay = 0.5 (seconds)"
-	echo "If no arguments are passed, pid and run_user will be found automatically"
+	echo "If no arguments are passed, pid will be found automatically"
 	exit 0
 else
 pid=$1			# required
-user=$2			# required
-count=${3:-10}	# defaults to 10 times
-delay=${4:-0.5}	# defaults to 0.5 seconds
+count=${2:-10}	# defaults to 10 times
+delay=${3:-0.5}	# defaults to 0.5 seconds
 
 fi
-if [ $1 !='--help' ] && [ $1 !='-h' ];
+
+if [[ $1 != '--help' ]] && [[ $1 != '-h' ]];
 then
 	while [ $count -gt 0 ]
 	do
